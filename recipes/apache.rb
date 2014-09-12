4fdb5eeef25119ac1f73d2275fabe373cc0a0eef
Damian Szymanski <damian@assembla.com>
2014-09-12 11:29:52 +0200

    Fix for vname

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -25,7 +25,7 @@ apache_site '000-default' do
   enable false
 end
 
-template "#{node['apache']['dir']}/sites-available/#{node['nagios']['server']['vname']}" do
+template "#{node['apache']['dir']}/sites-available/#{node['nagios']['server']['vname']}.conf" do
   source 'apache2.conf.erb'
   mode '0644'
   variables(
@@ -35,7 +35,7 @@ template "#{node['apache']['dir']}/sites-available/#{node['nagios']['server']['v
     :ssl_cert_file => node['nagios']['ssl_cert_file'],
     :ssl_cert_key  => node['nagios']['ssl_cert_key']
   )
-  if File.symlink?("#{node['apache']['dir']}/sites-enabled/#{node['nagios']['server']['vname']}")
+  if File.symlink?("#{node['apache']['dir']}/sites-enabled/#{node['nagios']['server']['vname']}.conf")
     notifies :reload, 'service[apache2]'
   end
 end
